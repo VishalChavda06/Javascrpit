ec920a823da89af0d457ee26680084c4618f01c5
Vishal Chavda <Lenovo@VISHU.HIRAL>
2025-01-29 17:48:17 +0530

    add a code

---
+++ lectures/lec.1.js
@@ -0,0 +1,15 @@
+console.log("Vishal Chavda")
+
+let a = 10;
+let b = 20;
+let c = "30";
+
+console.log(a+b)
+
+console.log(typeof a , typeof b , typeof c)
+
+console.log(a + b - Number(c));
+
+console.log(typeof a + typeof b + typeof c); 
+
+console.log(a + b - c)
