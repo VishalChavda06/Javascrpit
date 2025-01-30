b0da63ab4b2390330c360a247c586c936e11ec0e
Vishal Chavda <vc229067@gmail.com>
2025-01-30 11:48:22 +0530

    Update lec.1.js

--- lectures/lec.1.js
+++ lectures/lec.1.js
@@ -1,15 +1,30 @@
-console.log("Vishal Chavda")
+console.log("Vishal Chavda");
+
+let x = 12;
+let y = 12;
+const z = "12";
 
 let a = 10;
-let b = 20;
-let c = "30";
+let b = 15;
 
-console.log(a+b)
+// Add a Value 
+x = 10;
+y -= 1;
 
-console.log(typeof a , typeof b , typeof c)
-
-console.log(a + b - Number(c));
-
-console.log(typeof a + typeof b + typeof c); 
-
-console.log(a + b - c)
+// sum - min
+console.log(x + y - z);
+// Cheak variable type
+console.log(typeof x, typeof y, typeof z);
+// Cheak varible equal 
+console.log(x ==y);
+// Add a value of variable
+// 12
+// 13
+console.log(x++);
+console.log(x);
+// less than and geter than 
+console.log(x > y);
+// Not equal to
+console.log(x != y);
+// And - Or
+console.log(x == y || a == b);
