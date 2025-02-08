d483d0f95e1a4d82f99584e3e2163853f1b21cb3
Vishal Chavda <vc229067@gmail.com>
2025-02-08 09:31:54 +0530

    Update index.js

--- Tutorial/q31/index.js
+++ Tutorial/q31/index.js
@@ -2,5 +2,7 @@ function change() {
   let a = document.getElementById("vauleofA").value;
   result = document.getElementById("result").innerHTML = a.toUpperCase();
 }
-
-
+function lower() {
+  let a = document.getElementById("vauleofA").value;
+  result = document.getElementById("result").innerHTML = a.toLowerCase();
+}
