7ed6fd6aeed966554875b9d5e62858c4e7b67f2d
Vishal Chavda <vc229067@gmail.com>
2025-02-10 15:53:14 +0530

    Update q30.js

--- Tutorial/q30.js
+++ Tutorial/q30.js
@@ -1,11 +1,16 @@
-function hasVowel(str) {
-  for (let i = 0; i < str.length; i++) {
-    if ("aeiou".includes(str[i])) {
-      return true; // Found a vowel
-    }
+let a = "red";
+let n = a.length;
+let result = false;
+
+for (let i = 0; i < n; i++) {
+  if (a[i] == "a" || a[i] == "e" || a[i] == "o" || a[i] == "u" || a[i] == "i") {
+    result = true;
+    break;
   }
-  return false; // No vowels found
 }
 
-console.log(hasVowel("red")); // true
-console.log(hasVowel("xyz")); // false
+if (result == true) {
+  console.log("true");
+} else {
+  console.log("false");
+}
