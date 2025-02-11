3309bd396cfe27db3f47d63afaa82971a481ac57
Vishal Chavda <vc229067@gmail.com>
2025-02-11 14:44:53 +0530

    Update script.js

--- Tutorial/Exam-1/script.js
+++ Tutorial/Exam-1/script.js
@@ -5,9 +5,9 @@ function calculateBill() {
   if (use >= 0 && use <= 50) {
     amount = use * 5 + 100;
   } else if (use > 50 && use <= 100) {
-    amount = (use - 50) * 10 + 350;
+    amount = (use - 50) * 10 + 5 * 50 + 100;
   } else if (use > 100 && use <= 150) {
-    amount = (use - 100) * 15 + 850;
+    amount = (use - 100) * 15 + 5 * 50 + 50 * 10 + 100;
   } else {
     amount = (use - 150) * 20 + 1600;
     // return;
