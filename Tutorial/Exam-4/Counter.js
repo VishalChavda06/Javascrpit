c2ef7dff3b1f72ffb66a0aaaf650ee1acc699ca2
Vishal Chavda <vc229067@gmail.com>
2025-03-04 15:32:08 +0530

    Update Counter.js

--- Tutorial/Exam-4/Counter.js
+++ Tutorial/Exam-4/Counter.js
@@ -2,12 +2,10 @@ let count = 0;
 let Stop = null;
 
 function startCounter() {
-  if (!Stop) {
-    Stop = setInterval(() => {
-      count++;
-      document.getElementById("counter").innerHTML = count;
-    }, 100);
-  }
+  Stop = setInterval(() => {
+    count++;
+    document.getElementById("counter").innerHTML = count;
+  }, 100);
 }
 
 function stopCounter() {
@@ -20,5 +18,3 @@ function resetCounter() {
   count = 0;
   document.getElementById("counter").innerHTML = count;
 }
-
-
