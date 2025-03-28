e43f3a35f22de113b932e14d3dbe44b90d03818b
Vishal Chavda <vc229067@gmail.com>
2025-03-28 03:16:01 -0700

    Update a Login&SignUP Page

---
+++ Clothing/Login/Log.js
@@ -0,0 +1,42 @@
+let Users = JSON.parse(localStorage.getItem("Users")) || [];
+
+const IsExist = (email) => {
+  let isuser = Users.find((ele) => ele.email === email);
+  if (isuser) {
+    return { found: true, user: isuser };
+  } else {
+    return { found: false, user: isuser };
+  }
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const emailerror = document.getElementById("email-error");
+  const passregex = document.getElementById("password-error");
+  const SuccessFully = document.getElementById("success");
+  let user = {
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  };
+  let userData = IsExist(user.email);
+
+  console.log(userData);
+  if (userData.found) {
+    if (userData.user.password == user.password) {
+      // alert("Login SuccessFully...");
+      SuccessFully.style.display = "inline-block";
+      SuccessFully.style.transition = "all 0.5s ease-in-out";
+      document.getElementById("authForm").reset();
+    } else {
+      // alert("Invalid password");
+      passregex.innerHTML = "Invalid Password";
+      passregex.style.color = "red";
+    }
+  } else {
+    // alert("Invalid email");
+    emailerror.innerHTML = "Invalid email";
+    emailerror.style.color = "red";
+  }
+};
+
+document.getElementById("authForm").addEventListener("submit", handleSubmit);
---
+++ Clothing/Login/SignUp.js
@@ -0,0 +1,81 @@
+let Users = JSON.parse(localStorage.getItem("Users")) || [];
+
+const IsExist = (email) => {
+  let isuser = Users.find((ele) => ele.email === email);
+  if (isuser) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const newuser = document.getElementById("newuser");
+  const Alreday = document.getElementById("alreday");
+  let user = {
+    username: document.getElementById("username").value,
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  };
+  if (IsExist(user.email)) {
+    // alert("User Alreday Exist..");
+    Alreday.style.display = "inline-block";
+    return;
+  } else {
+    Users.push(user);
+    localStorage.setItem("Users", JSON.stringify(Users));
+    // alert("User Create A SuccessFully..");
+    newuser.style.display = "inline-block";
+    document.getElementById("authForm").reset();
+  }
+  //   console.log(user);
+};
+
+const validatename = () => {
+  const Username = username.value.trim();
+  const nameerror = document.getElementById("name-error");
+
+  if (Username.length < 4) {
+    nameerror.innerHTML = "Enter the Valid Name";
+    return false;
+  } else {
+    nameerror.innerHTML = "Valid Name";
+    nameerror.style.color = "green";
+    return true;
+  }
+};
+const validateemail = () => {
+  const Email = email.value.trim();
+  const emailerror = document.getElementById("email-error");
+
+  if (!/^\S+@\S+\.\S+$/.test(Email)) {
+    emailerror.innerHTML = "Invalid email format";
+    return false;
+  } else {
+    emailerror.innerHTML = "valid email";
+    emailerror.style.color = "green";
+    return true;
+  }
+};
+
+const validatepass = () => {
+  // const passregex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+  const Password = password.value.trim();
+  const passworderror = document.getElementById("password-error");
+
+  if (Password.length < 4) {
+    passworderror.innerHTML = "Enter a Valid Password";
+    return false;
+  } else {
+    passworderror.innerHTML = "valid Password";
+    passworderror.style.color = "green";
+    return true;
+  }
+};
+
+username.addEventListener("input", validatename);
+password.addEventListener("input", validatepass);
+email.addEventListener("input", validateemail);
+
+document.getElementById("authForm").addEventListener("submit", handleSubmit);
